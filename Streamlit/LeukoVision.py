b4bfbb84bff33db80a594d245b5c91b866641098
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-07 17:18:00 +0200

    update

--- Streamlit/LeukoVision.py
+++ Streamlit/LeukoVision.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    /* Target the nav bar text */
+    .st-emotion-cache-1wbqy5l a p {
+        font-size:18px !important;   /* Increase/decrease font size */
+        font-weight:600;             /* Make bold */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 home = st.Page("./pages/Page1.py", title="LeukoVision", icon="🏠")
 pred = st.Page("./pages/Prediction.py", title="Predictions", icon="🧬")
 
