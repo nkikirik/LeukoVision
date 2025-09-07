c5be8e2ddcb5de7e07a174f5a6d3888f235fca6f
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-07 17:09:43 +0200

    update

--- Streamlit/LeukoVision.py
+++ Streamlit/LeukoVision.py
@@ -1,2 +1,17 @@
 import streamlit as st
 
+home = st.Page("./Streamlit/pages/Page1.py", title="LeukoVision", icon="🏠")
+pred = st.Page("./Streamlit/pages/Prediction.py", title="Predictions", icon="🧬")
+
+
+# Navigation container
+pg = st.navigation(
+    {
+        "Main": [home],
+        "Models": [pred],
+    },
+    position="top",  # 👈 horizontal navbar
+)
+
+# Run the active page
+pg.run()
