22d01bdbbd98177d92157cf4033c50d089b516bf
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-08 16:31:53 +0200

    update

--- Streamlit/pages/home.py
+++ Streamlit/pages/home.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("🏠 Welcome to LeukoVision")
 
 st.markdown("""
+            <div style="text-align: justify;">
 **LeukoVision** is an advanced, AI-powered platform designed to analyze and classify blood cell images with precision.  
 Leveraging state-of-the-art deep learning models — **InceptionV3**, **ResNet50**, and **VGG16** — the app provides accurate identification of various blood cell types, including basophils, eosinophils, erythroblasts, lymphocytes, monocytes, neutrophils, and platelets.
 
