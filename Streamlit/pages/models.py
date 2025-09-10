f2753c6515bc11780091938326ed7e0da99d49d6
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-10 15:27:49 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -21,7 +21,7 @@ section = option_menu(
 
 if section == "InceptionV3":
     st.header("InceptionV3 🔬")
-    st.image('./Streamlit/pages/images/inceptionv3.png', caption='Architecture diagram of InceptionV3')
+    st.image('./Streamlit/pages/images/inceptionv3.png', caption='Architecture diagram of InceptionV3',use_container_width=True)
 
 elif section == "ResNet50":
     st.header("ResNet50 🧬")
