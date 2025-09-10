47d6ab8693a7d3149482474cade9337ae80b7b10
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-10 17:09:14 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -27,8 +27,8 @@ if section == "InceptionV3":
     st.header("InceptionV3 🔬")
     st.image(white_bg('./Streamlit/pages/images/inceptionv3.png'), caption='Architecture diagram of InceptionV3',use_container_width=True)
     model = InceptionV3(weights='imagenet')
-    text=model.summary()
-    st.text(text)
+    with st.expander("See Full Model Summary"):
+        st.text(model.summary())
 
 elif section == "ResNet50":
     st.header("ResNet50 🧬")
